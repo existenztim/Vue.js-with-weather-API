1e50967fa18228e51dc99ab385c8635ca699d3c6
existenztim <tim920129@hotmail.com>
2023-01-18 00:14:10 +0100

    :tada: Added current weather type

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <main>
     <div class="search-box">
-      <input class="search-bar" type="text" placeholder="Search..."/>
+      <input class="search-bar"
+        type="text"
+        placeholder="Search..."
+        v-model="query"/> <!--binds it to {query} var-->
     </div>
-    <div class ="query">{{ query }}</div>
+    <div class ="query">{{ query }} {{ country }}</div> <!--might capitalize first letter-->
     <div class ="temp">{{ temp }}</div>
+    <div class ="weather">{{ weather }}</div>
   </main>
   <!--<HelloWorld msg="Vite + Vue" /> self closing tag-->
 </template>
@@ -16,21 +20,23 @@ import { ref } from 'vue';
 
 const apiKey = '7453b8cce1610c8d5caa675608b66681';
 const url = 'https://api.openweathermap.org/data/2.5/';
-const query = 'stockholm';
+const query = 'Stockholm';
 const units = 'metric';
-const temp = ref();
-// https://github.com/TylerPottsDev/weather-vue/blob/master/src/App.vue
+const country = ref('');
+const temp = ref('');
+const weather = ref('');
+
 const fetchWheater = () => {
-  fetch(`${url}weather?q=${query}&units=${units}&APPID=${apiKey}`)
+  fetch(`${url}weather?q=${query}&units=${units}&APPID=${apiKey}`) // the query string
     .then((res) => {
       console.log(res);
       return res.json();
     })
     .then((json) => {
-      console.log(json);
-      console.log(json.main.temp);
       temp.value = json.main.temp;
-      temp.value += ' grader';
+      country.value = json.sys.country;
+      weather.value = json.weather[0].main;
+      temp.value += ' °c';
     })
     .catch((err) => {
       console.error(err);
@@ -41,15 +47,5 @@ fetchWheater();
 </script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
