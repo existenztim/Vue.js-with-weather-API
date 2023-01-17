af7213b9d742b23f0042851a6a8ac718e5637f66
existenztim <tim920129@hotmail.com>
2023-01-17 23:33:46 +0100

    :tada: Degree is displayed in HTML!

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,44 @@
-<script setup> /** notera setup, fungerar
- * utan men blir annat syntax, setup mer "fritt", setup är mer likt react syntax */
-import HelloWorld from './components/HelloWorld.vue'; // behöver ej importa om komponenten ej har script
+<template>
+  <main>
+    <div class="search-box">
+      <input class="search-bar" type="text" placeholder="Search..."/>
+    </div>
+    <div class ="query">{{ query }}</div>
+    <div class ="temp">{{ temp }}</div>
+  </main>
+  <!--<HelloWorld msg="Vite + Vue" /> self closing tag-->
+</template>
+
+<script setup>
+import { ref } from 'vue';
+// import HelloWorld from './components/HelloWorld.vue';
+// behöver ej importa om komponenten ej har script
 
 const apiKey = '7453b8cce1610c8d5caa675608b66681';
 const url = 'https://api.openweathermap.org/data/2.5/';
 const query = 'stockholm';
+const units = 'metric';
+const temp = ref();
 // https://github.com/TylerPottsDev/weather-vue/blob/master/src/App.vue
 const fetchWheater = () => {
-  fetch(`${url}weather?q=${query}&units=metric&APPID=${apiKey}`)
+  fetch(`${url}weather?q=${query}&units=${units}&APPID=${apiKey}`)
     .then((res) => {
       console.log(res);
       return res.json();
     })
     .then((json) => {
       console.log(json);
+      console.log(json.main.temp);
+      temp.value = json.main.temp;
+      temp.value += ' grader';
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
     });
 };
+fetchWheater();
 
-// fetchWheater();
 </script>
-
-<template>
-
-  <HelloWorld msg="Vite + Vue" /> <!--self closing tag-->
-  <WeatherCards/> <!--det går att ge classer till dessa, så man kan styla i App.vue-->
-</template>
 
 <style scoped>
 .logo {
