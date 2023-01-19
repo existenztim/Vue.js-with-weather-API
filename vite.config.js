dd5d0bce3002e3a9cfd1a1a217cde145d292f269
existenztim <tim920129@hotmail.com>
2023-01-20 00:19:15 +0100

    :zap: Solution?

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/existenztim/Vue.js-with-weather-API/',
+  base: '/Vue.js-with-weather-API/',
   plugins: [vue()],
+  define: {
+    'process.env': {},
+  },
 });
